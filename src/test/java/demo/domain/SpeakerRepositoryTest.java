72afb20810b5c627db4026718b40f6bb402f575f
Stephane Nicoll <snicoll@pivotal.io>
2015-04-14 12:31:01 +0200

    Initial import

---
+++ src/test/java/demo/domain/SpeakerRepositoryTest.java
@@ -0,0 +1,26 @@
+package demo.domain;
+
+import demo.DemoApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DemoApplication.class)
+public class SpeakerRepositoryTest {
+
+	@Autowired
+	private SpeakerRepository repository;
+
+	@Test
+	public void testFindByTwitter() throws Exception {
+		Speaker dave = repository.save(new Speaker("Dave", "Syer", "david_syer"));
+		assertThat(repository.findByTwitter("david_syer").getId(), is(dave.getId()));
+	}
+}
